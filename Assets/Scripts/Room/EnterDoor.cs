f3e74bcc0fc2629d869c0eb75db1d2ec59a6e629
Chevancey <78435665+Chevancey@users.noreply.github.com>
2023-02-23 01:27:55 +0100

    [Shader] Added door shader effect after enemies are killed

--- Assets/Scripts/Room/EnterDoor.cs
+++ Assets/Scripts/Room/EnterDoor.cs
@@ -5,18 +5,25 @@ using UnityEngine;
 public class EnterDoor : MonoBehaviour
 {
     public GameObject door;
+    public MeshRenderer Mesh;
+    private Material[] SkinnedMaterials;
+
+    [SerializeField]
+    private float dissolveRate = 0.0125f;
+
+    [SerializeField]
+    private float refreshRate = 0.025f;
 
     private void Start()
     {
-        EnemyManager.Instance.OnAllEnnemiesKilled += OpenDoor;
-    }
+        Mesh = door.GetComponent<MeshRenderer>();
 
-    private void OpenDoor()
-    {
-        if (door != null) 
+        if (Mesh != null)
         {
-            door.SetActive(false);
+            SkinnedMaterials = Mesh.materials;
         }
+
+        EnemyManager.Instance.OnAllEnnemiesKilled += StartOpenDoor;
     }
 
     private void OnTriggerEnter(Collider other)
@@ -25,5 +32,32 @@ public class EnterDoor : MonoBehaviour
         {
             RoomManager.instance.LoadRandomLevel();
         }
+    }
+    private void StartOpenDoor()
+    {
+        if (door != null)
+        {
+            StartCoroutine(OpenDoor());
+        }
+    }
+
+    IEnumerator OpenDoor()
+    {
+        if (SkinnedMaterials.Length > 0)
+        {
+            float counter = 0;
+
+            while (SkinnedMaterials[0].GetFloat("_DissolveAmount") < 1)
+            {
+                counter += dissolveRate;
+                for (int i = 0; i < SkinnedMaterials.Length; i++)
+                {
+                    SkinnedMaterials[0].SetFloat("_DissolveAmount", counter);
+                }
+                yield return new WaitForSeconds(refreshRate);
+            }
+        }
+        //OPEN GNE GNOOOOOR
+        door.SetActive(false);
     }
 }
