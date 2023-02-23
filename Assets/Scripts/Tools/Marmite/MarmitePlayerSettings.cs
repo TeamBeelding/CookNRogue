15bb9f8e1ee57376a236fcb326c133575ece464a
Antoine Jeanselme <67123340+ajeanselme@users.noreply.github.com>
2023-02-23 12:00:20 +0100

    Marmite basics

---
+++ Assets/Scripts/Tools/Marmite/MarmitePlayerSettings.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+public class MarmitePlayerSettings
+{
+    [Button] private String text;
+}
