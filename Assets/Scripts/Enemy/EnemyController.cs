7cd1156cd7d0bde9182125c861eae38f4f63fe4f
Elvis <elvishenry2402@gmail.com>
2023-03-09 20:12:59 +0100

    RRRRRAAAAAAAAAAAAAAH

--- Assets/Scripts/Enemy/EnemyController.cs
+++ Assets/Scripts/Enemy/EnemyController.cs
@@ -69,10 +69,8 @@ public abstract class EnemyController : MonoBehaviour, IState, IEffectable
     // Update is called once per frame
     protected void Update()
     {
-        // if (state == State.Dying)
-        //     return;
-        
-        // IStateManagement();
+        if (_effectData != null)
+            HandleEffect();
     }
 
     public abstract bool IsMoving();
@@ -195,7 +193,7 @@ public abstract class EnemyController : MonoBehaviour, IState, IEffectable
         if(_effectData._DOTAmount != 0 && _currentEffectTime > _NextTickTime)
         {
             _NextTickTime += _currentEffectTime;
-            // ReduiceHealth(_effectData._DOTAmount);
+            TakeDamage(_effectData._DOTAmount);
         }
             
     }
