11e37e4805b9dc79eab6eeea98f18582ebdb1102
Elvis Henry <elvishenry2402@gmail.com>
2023-03-21 10:05:48 +0100

    [Add player reference SO]

--- Assets/Scripts/Reference/PlayerReference.cs
+++ Assets/Scripts/Reference/PlayerReference.cs
@@ -2,17 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerReference : MonoBehaviour
+[CreateAssetMenu(menuName = "Reference/Player Reference")]
+public class PlayerReference : Reference<Entity>
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
