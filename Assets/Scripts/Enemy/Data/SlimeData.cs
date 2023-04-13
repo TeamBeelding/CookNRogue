86024d018f1b2a398f3e31989e9e4800e6ea9146
Elvis <elvishenry2402@gmail.com>
2023-04-13 10:37:24 +0200

    Some push

--- Assets/Scripts/Enemy/Data/SlimeData.cs
+++ Assets/Scripts/Enemy/Data/SlimeData.cs
@@ -1,8 +1,4 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Serialization;
 
 [CreateAssetMenu(fileName = "SlimeData", menuName = "Enemy/SlimeData")]
 public class SlimeData : ScriptableObject
