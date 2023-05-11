cecac87683ff25996313406f33eba52de1cd271a
Lych <70584222+Lych30@users.noreply.github.com>
2023-05-11 22:16:22 +0200

    Dialogue updated and vfx

--- Assets/Scripts/Dialogues/DialogueBox.cs
+++ Assets/Scripts/Dialogues/DialogueBox.cs
@@ -22,15 +22,18 @@ public class DialogueBox : MonoBehaviour
 
     [SerializeField] Camera cam;
     [SerializeField] Transform Lookat;
+    [SerializeField] Vector3 _offset;
     private void Start()
     {
         cam = Camera.main;
+        DialogueText.text = "";
+        TimeBtwLetters = 1 / TextSpeed;
     }
     private void Update()
     {
         if(Lookat != null)
         {
-            Vector3 pos = cam.WorldToScreenPoint(Lookat.position  + Vector3.up);
+            Vector3 pos = cam.WorldToScreenPoint(Lookat.position  + _offset);
 
             if (transform.position != pos)
                 transform.position = pos;
@@ -49,8 +52,6 @@ public class DialogueBox : MonoBehaviour
         //INITIALISE THE TEXT TO DISPLAY AND THE PARAMETERS
         Lookat = lookat;
         textToDisplay = text;
-        DialogueText.text = "";
-        TimeBtwLetters = 1 / TextSpeed;
         DisplayEnumerable = Display(textToDisplay);
 
         //LAUNCHING THE DISPLAY OF THE TEXT
