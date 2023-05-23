7da2a74379ea85ca1fea2833f901b72453aa9fcf
Elvis Henry <elvishenry2402@gmail.com>
2023-05-23 09:41:40 +0200

    Throw
    Test Throw object

---
+++ Assets/Scripts/Enemy/Slime/ThrowObject.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+namespace Enemy.Slime
+{
+    public class ThrowObject : MonoBehaviour
+    {
+        [SerializeField] private AnimationCurve curve;
+        
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+        
+        private void Throw()
+        {
+            
+        }
+    }
+}
