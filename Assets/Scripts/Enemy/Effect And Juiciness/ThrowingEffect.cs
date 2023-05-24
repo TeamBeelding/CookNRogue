93126c255f992d78ff48c72a1d6a41a4190ed883
Elvis Henry <elvishenry2402@gmail.com>
2023-05-24 10:08:47 +0200

    ThrowObject

---
+++ Assets/Scripts/Enemy/Effect And Juiciness/ThrowingEffect.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using UnityEngine;
+
+namespace Enemy.Effect_And_Juiciness
+{
+    public class ThrowingEffect : MonoBehaviour
+    {
+        [SerializeField] private AnimationCurve xCurve;
+        [SerializeField] private AnimationCurve yCurve;
+        [SerializeField] private float speed;
+        [SerializeField] private float maxHeight;
+        [SerializeField] private float maxLength;
+        [SerializeField] private Vector3 direction;
+        [SerializeField] private Transform target;
+        
+        // Start is called before the first frame update
+        void Start()
+        {
+            direction.Normalize();
+        }
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.A))
+            {
+                StartCoroutine(ICurve());
+            }
+        }
+
+        private IEnumerator ICurve()
+        {
+            float timer = 0;
+            Vector3 initPos = transform.position;
+
+            while (timer < 1)
+            {
+                float x = xCurve.Evaluate(timer) * maxLength;
+                float y = yCurve.Evaluate(timer) * maxHeight;
+                
+                transform.position = initPos + direction * x + Vector3.up * y;
+                // transform.position = initPos + new Vector3(x, y, transform.position.z);
+                
+                timer += Time.deltaTime * speed;
+                
+                yield return new WaitForSeconds(Time.deltaTime * speed);
+            }
+        }
+    }
+}
