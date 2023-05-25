06aa9751ccc856de3be26ff24f03ab2b823fe8cb
Antoine Jeanselme <67123340+ajeanselme@users.noreply.github.com>
2023-05-25 10:33:56 +0200

    Tree color randomizer

--- Assets/Scripts/TreeRandomizer.cs
+++ Assets/Scripts/TreeRandomizer.cs
@@ -7,54 +7,37 @@ using Random = UnityEngine.Random;
 public class TreeRandomizer : MonoBehaviour
 {
     public TreeData[] trees;
-    public TreeSize forceSize;
-    
-    private MeshFilter _meshFilter;
-    
+
+    private MeshRenderer _meshRenderer;
+
 
     private void Awake()
     {
-        _meshFilter = GetComponent<MeshFilter>();
+        _meshRenderer = GetComponent<MeshRenderer>();
     }
 
     private void Start()
     {
-        var random = Random.Range(0, 3);
-        TreeSize treeSize = TreeSize.small;
-
-        if (random == 0) treeSize = TreeSize.small;
-        else if (random == 1) treeSize = TreeSize.medium;
-        else if (random == 2) treeSize = TreeSize.big;
-
-        List<TreeData> availableTrees = new List<TreeData>();
+        var random = Random.Range(0f, 1f);
+        var last = 0f;
         foreach (var treeData in trees)
         {
-            if(treeData.size == treeSize)
-                availableTrees.Add(treeData);
+            last += treeData.chance;
+            if (random <= last)
+            {
+                _meshRenderer.material = treeData.material;
+                break;
+            }
         }
-
-        random = Random.Range(0, availableTrees.Count);
-        _meshFilter.mesh = availableTrees[random].mesh;
     }
 
-    public enum TreeSize
-    {
-        small,
-        medium,
-        big
-    }
-    public enum TreeColor
-    {
-        green,
-        yellow,
-        red
-    }
     
     [Serializable]
     public class TreeData
     {
-        public TreeSize size;
-        public TreeColor color;
-        public Mesh mesh;
+        public Material material;
+        
+        [Range(0f,1f)]
+        public float chance;
     }
 }
