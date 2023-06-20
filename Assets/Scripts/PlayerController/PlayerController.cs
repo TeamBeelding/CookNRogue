5df12d2ae45bd45afdc6431df4e136ee1dd5eb42
Kvasirson <t.lenaerts@laposte.net>
2023-06-20 18:42:07 +0200

    [Fix] fixed scene load

--- Assets/Scripts/PlayerController/PlayerController.cs
+++ Assets/Scripts/PlayerController/PlayerController.cs
@@ -881,13 +881,13 @@ public class PlayerController : MonoBehaviour
 
     public void QuitGame()
     {
-        Application.Quit();
+        SceneManager.LoadScene(0);
     }
     
     
     public void RestartGame()
     {
-        SceneManager.LoadScene(1);
+        SceneManager.LoadScene(2);
     }
 
     public void EndGame()
