ae876a59751070a501687f23a75784ba62a4604b
Lych <70584222+Lych30@users.noreply.github.com>
2023-11-02 10:27:20 +0100

    ingredient pickup

--- Assets/Scripts/PlayerController/Crafting/Ingredient.cs
+++ Assets/Scripts/PlayerController/Crafting/Ingredient.cs
@@ -4,10 +4,17 @@ public class Ingredient : MonoBehaviour, IInteractable
 {
     [SerializeField]
     ProjectileData _projectileData;
-
+    [SerializeField]
+    ParticleSystem _particles;
     public ProjectileData GetData
     {
         get => _projectileData;
+    }
+
+    void Start()
+    {
+        _particles = transform.GetChild(2).GetComponentInChildren<ParticleSystem>();
+        _particles.transform.parent = null;
     }
 
     public void Interactable(bool isInteractable)
@@ -25,7 +32,29 @@ public class Ingredient : MonoBehaviour, IInteractable
         if(other.CompareTag("Player"))
         {
             PlayerCookingInventory.Instance.AddIngredientToInventory(_projectileData);
+            OnPickUpIngredientParticles();
             Destroy(gameObject);
         }
     }
+
+    private void OnPickUpIngredientParticles()
+    {
+        ParticleSystem.MainModule main = _particles.main;
+        main.simulationSpeed = 4;
+
+        var emission = _particles.emission;
+        emission.enabled = false;
+
+        var vol = _particles.velocityOverLifetime; ;
+        vol.y = 1.5f;
+
+        var noise = _particles.noise;
+        noise.strengthX = 2;
+        noise.strengthY = 2;
+        noise.strengthZ = 2;
+
+        Destroy(transform.parent,1);
+    }
+
+    
 }
