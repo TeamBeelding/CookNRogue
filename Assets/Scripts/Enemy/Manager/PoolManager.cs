4a137a2235e4fcfb46dd37d559718f5fb8a14a43
Elvis Henry <elvishenry2402@gmail.com>
2023-11-08 11:51:03 +0100

    Pooling system start

---
+++ Assets/Scripts/Enemy/Manager/PoolManager.cs
@@ -0,0 +1,21 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoolManager : MonoBehaviour
+{
+    // singleton
+    // reference to all queue
+    // fonction to pull some obj
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
