a720efa0e0be214ce78295163fe63b19aa6b8681
Lych <Aqu4men@gmail.com>
2023-11-15 16:25:43 +0100

    clean up and begin missiles decals

--- Assets/Scripts/Decal/BossDecal/MissileDecal.cs
+++ Assets/Scripts/Decal/BossDecal/MissileDecal.cs
@@ -1,19 +1,17 @@
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Rendering.Universal;
-using UnityEngine.UIElements;
 
-public class BulletDecal : MonoBehaviour
+public sealed class MissileDecal : MonoBehaviour
 {
     [Header("Projector")]
     [SerializeField] DecalProjector _decalProjector;
 
     [Header("Scale Values")]
-    [SerializeField] float _minScale;
-    [SerializeField] float _maxScale;
+    [SerializeField] float _Scale;
     [SerializeField] AnimationCurve _scaleCurve;
     [SerializeField] float _ScalingSpeed;
-    float scale;
 
     [Header("Alpha Values")]
     [SerializeField] float _minAlpha;
@@ -22,21 +20,38 @@ public class BulletDecal : MonoBehaviour
     float _alpha;
     [SerializeField] AnimationCurve _alphaCurve;
 
-    
-    Color _decalColor;
 
+    Color _decalColor;
     public void Init(Color color)
     {
         StopAllCoroutines();
         float randomRot = Random.Range(-360.0f, 360.0f);
-        transform.rotation = Quaternion.Euler(0,randomRot,0);
-        float randomScale = Random.Range(_minScale,_maxScale);
-        scale = randomScale;
-        transform.localScale = new Vector3(scale, scale, 1);
-        StartCoroutine(decalFade(color));
+        transform.rotation = Quaternion.Euler(0, randomRot, 0);
+
+        transform.localScale = new Vector3(_Scale, _Scale, 1);
+        StartDecalScaleFade(color);
     }
-   
-    IEnumerator decalFade(Color color)
+
+    public void StartDecalScaleFade(Color color)
+    {
+        StartCoroutine(Scale());
+        StartCoroutine(Fade(color));
+    }
+
+    IEnumerator Scale()
+    {
+        //SCALE
+        float scaleProgress = 0;
+        while (scaleProgress < 1)
+        {
+            float newScale = _Scale * _scaleCurve.Evaluate(scaleProgress);
+            transform.localScale = new Vector3(newScale, newScale, 1);
+            scaleProgress += Time.deltaTime * _ScalingSpeed;
+            yield return new WaitForEndOfFrame();
+        }
+    }
+
+    IEnumerator Fade(Color color)
     {
         _decalProjector.material = new Material(_decalProjector.material);
         var mat = _decalProjector.material;
@@ -44,16 +59,6 @@ public class BulletDecal : MonoBehaviour
         color.a = _alpha;
         _decalColor = color;
         mat.SetColor("_Color", _decalColor);
-
-        //SCALE
-        float scaleProgress = 0;
-        while (scaleProgress < 1)
-        {
-            float newScale = scale * _scaleCurve.Evaluate(scaleProgress);
-            transform.localScale = new Vector3(newScale, newScale, 1);
-            scaleProgress += Time.deltaTime * _alphaFadeSpeed;
-            yield return new WaitForEndOfFrame();
-        }
 
         //FADE
         float alphaProgress = 0;
@@ -68,5 +73,4 @@ public class BulletDecal : MonoBehaviour
 
         gameObject.SetActive(false);
     }
-
 }
