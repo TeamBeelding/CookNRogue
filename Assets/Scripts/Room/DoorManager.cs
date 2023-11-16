61175d816a01c24fdc65c6a432b8ec933f7274fd
Antoine Jeanselme <67123340+ajeanselme@users.noreply.github.com>
2023-11-16 10:57:38 +0100

    Navmesh build at runtime if not baked

--- Assets/Scripts/Room/DoorManager.cs
+++ Assets/Scripts/Room/DoorManager.cs
@@ -1,12 +1,29 @@
+using Unity.AI.Navigation;
 using UnityEngine;
 
 public class DoorManager : MonoBehaviour
 {
     private EnterDoor[] _doorsInScene;
+    private NavMeshSurface[] _navMeshSurfaces;
     private GameObject _player;
 
-    private void Start()
+    private void Awake()
     {
+        _navMeshSurfaces = FindObjectsOfType<NavMeshSurface>();
+        if (_navMeshSurfaces.Length == 0)
+        {
+            Debug.LogWarning("No NavMesh prefab fund in scene, enemies won't work");
+        }
+
+        foreach (var navMeshSurface in _navMeshSurfaces)
+        {
+            if (navMeshSurface.navMeshData == null)
+            {
+                Debug.LogWarning("NavMesh data not baked. It will be generated at runtime, but bake in editor for optimization");
+                navMeshSurface.BuildNavMesh();
+            }
+        }
+
         _doorsInScene = FindObjectsOfType<EnterDoor>();
         _player = GameObject.FindGameObjectWithTag("Player");
 
