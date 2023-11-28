ab4112373f89fe60550fa34fb68dcb659124cf07
Remy-Schlaeintzauer <remybrighi@gmail.com>
2023-11-28 11:44:28 +0100

    edit LD

--- Assets/Scripts/Enemy/Manager/WaveManager.cs
+++ Assets/Scripts/Enemy/Manager/WaveManager.cs
@@ -52,6 +52,7 @@ public class WaveManager : MonoBehaviour
                 break;
             case State.EndWave:
                 EnemyManager.Instance.EndWave();
+                gameObject.SetActive(false);
                 break;
         }
     }
