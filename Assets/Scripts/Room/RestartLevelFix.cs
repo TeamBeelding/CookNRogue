55add086a52e0ad168f4c3df46fd44ed4efb4d33
Kvasirson <70716744+Kvasirson@users.noreply.github.com>
2023-12-21 10:49:00 +0100

    Last fixes for build

--- Assets/Scripts/Room/RestartLevelFix.cs
+++ Assets/Scripts/Room/RestartLevelFix.cs
@@ -26,20 +26,23 @@ public class RestartLevelFix : MonoBehaviour
 
     public void LoadScene(int index) 
     {
-        //m_transition.LoadTransition();
+        /*m_transition.LoadTransition();
 
         StartCoroutine(ILoadNextScene());
 
         IEnumerator ILoadNextScene()
         {
             DestroyInstances();
-            yield return new WaitForSeconds(0.1f);
             SceneManager.LoadScene(index);
-        }
+        }*/
+
+        DestroyInstances();
+        SceneManager.LoadScene(index);
     }
 
     void DestroyInstances()
     {
+        PoolManager.Instance.DestroyAI();
         Destroy(PlayerController.Instance.gameObject);
         Destroy(EnemyManager.Instance.gameObject);
         Destroy(PoolManager.Instance.gameObject);
